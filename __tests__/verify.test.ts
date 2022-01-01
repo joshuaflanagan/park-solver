7e9f194145959a020d46d06b26ec9434f9e4e5fd
Joshua Flanagan <joshuaflanagan@gmail.com>
2022-01-01 10:23:41 -0600

    Construct a valid board

--- __tests__/verify.test.ts
+++ __tests__/verify.test.ts
@@ -1,7 +1,114 @@
-import { sampleFunction } from "../src/index";
+import { Board } from "../src/index";
 
-describe("This is a simple test", () => {
-  test("Check the sampleFunction function", () => {
-    expect(sampleFunction("hello")).toEqual("hellohello");
+describe("Building a Board", () => {
+  const validRegionSpec = [
+    ["a", "a", "a", "b", "b"],
+    ["a", "c", "c", "c", "b"],
+    ["d", "c", "c", "e", "b"],
+    ["d", "d", "c", "e", "b"],
+    ["d", "c", "c", "e", "e"],
+  ];
+
+  test("A valid region has same number of regions, rows, and columns", () => {
+    const board = new Board(validRegionSpec);
+    expect(board.size).toEqual(5);
+  });
+
+  test("Defaults to requiring one fill per region, row, and column", () => {
+    const board = new Board(validRegionSpec);
+    expect(board.fillCount).toEqual(1);
+  });
+
+  test("Allow specifying the need for more fills", () => {
+    const board = new Board(validRegionSpec, 3);
+    expect(board.fillCount).toEqual(3);
+  });
+
+  test("Has 'size x size' cells", () => {
+    const board = new Board(validRegionSpec);
+    expect(board.cells.length).toEqual(5 * 5);
+  });
+
+  test("Each cell is associated with a region", () => {
+    const board = new Board(validRegionSpec);
+    expect(board.cells[0].region.id).toEqual("a");
+    expect(board.cells[3].region.id).toEqual("b");
+    expect(board.cells[6].region.id).toEqual("c");
+  });
+
+  test("A valid board has 'size' regions", () => {
+    const board = new Board(validRegionSpec);
+    expect(Object.keys(board.regions).length).toEqual(board.size);
+  });
+
+  test("Each cell is associated with a Row", () => {
+    const board = new Board(validRegionSpec);
+    expect(board.cells[0].row.id).toEqual("0");
+    expect(board.cells[1].row.id).toEqual("0");
+    expect(board.cells[6].row.id).toEqual("1");
+    expect(board.cells[10].row.id).toEqual("2");
+    expect(board.cells[15].row.id).toEqual("3");
+    expect(board.cells[24].row.id).toEqual("4");
+  });
+
+  test("A valid board has 'size' rows", () => {
+    const board = new Board(validRegionSpec);
+    expect(board.rows.length).toEqual(board.size);
+  });
+
+  test("Each cell is associated with a Col", () => {
+    const board = new Board(validRegionSpec);
+    expect(board.cells[0].col.id).toEqual("0");
+    expect(board.cells[1].col.id).toEqual("1");
+    expect(board.cells[4].col.id).toEqual("4");
+    expect(board.cells[5].col.id).toEqual("0");
+    expect(board.cells[7].col.id).toEqual("2");
+    expect(board.cells[13].col.id).toEqual("3");
+  });
+
+  test("A valid board has 'size' cols", () => {
+    const board = new Board(validRegionSpec);
+    expect(board.cols.length).toEqual(board.size);
+  });
+
+  test("Building a board throws if the number of regions does not match 'size'", () => {
+    const invalidRegionSpec = [ // a-f, 6 regions
+      ["a", "a", "a", "b", "b"],
+      ["a", "c", "c", "c", "b"],
+      ["d", "c", "c", "e", "b"],
+      ["d", "d", "c", "e", "b"],
+      ["d", "c", "c", "f", "f"],
+    ];
+
+    expect( () => {
+      new Board(invalidRegionSpec);
+    }).toThrow("regions");
+  });
+
+  test("Building a board throws if all rows dont have the same number of cells", () => {
+    const invalidRegionSpec = [
+      ["a", "a", "a", "b", "b"],
+      ["a", "c", "c", "c", "b"],
+      ["d", "c", "c", "e"     ], // only 4 cells
+      ["d", "d", "c", "e", "b"],
+      ["d", "c", "c", "e", "e"],
+    ];
+
+    expect( () => {
+      new Board(invalidRegionSpec);
+    }).toThrow("row");
+  });
+
+  test("Building a board throws if number of rows does not match number of columsn", () => {
+    const invalidRegionSpec = [
+      ["a", "a", "a", "b", "b"],
+      ["a", "c", "c", "c", "b"],
+      ["d", "c", "c", "e", "b"],
+      ["d", "c", "c", "e", "e"],
+    ];
+
+    expect( () => {
+      new Board(invalidRegionSpec);
+    }).toThrow("row");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,121 @@
-export function sampleFunction(x: string): string {
-  return x + x;
+/*
+ * State - array of cell states: free, blocked, full
+ * Board
+ *  - init with
+ *      "size" - length of one side. num cells will be size * size
+ *      = NO - instead of size, pass an array of "regions" (array of coords?) - the number of regions == size
+ *      = NO - pass an array of arrays of characters. each region is represented by a unique character
+ *      [
+ *        ["a", "a", "a", "b", "b"],
+ *        ["a", "c", "c", "c", "b"],
+ *        ["d", "c", "c", "e", "b"],
+ *        ["d", "d", "c", "e", "b"],
+ *        ["d", "c", "c", "e", "e"],
+ *       ]
+ *      "fillcount" - number of trees to resolve each area. start with 1
+ *
+ *  - enumerable cells
+ *  - array of rows
+ *  - array of columns
+ *  - array of regions
+ * Cell
+ *  - belongs to row
+ *  - belongs to column
+ *  - belongs to region
+ * Column
+ *  - array of cells
+ *  - numfree(state) - returns number of free cells. useful to check for 1
+ *  - resolved(state): bool - true if it has the required number of fulls
+ *  (can it ask its board for required number of fulls?)
+ * Row
+ *  - array of cells
+ *  - numfree(state)
+ *  - resolved(state): bool - true if it has the required number of fulls
+ * Region
+ *  - array of cells
+ *  - numfree(state)
+ *  - isLine(state) - if it is inline, return the col or row - block out any cell not belonging to this region
+ *  - resolved(state): bool - true if it has the required number of fulls
+ *
+ * for each free cell, create a state from current + this one full
+ *   now check 
+ *   - any (unresolved) region left without empty cells
+ *   - any (unresolved) row left without empty cells
+ *   - any (unresolved) col left without empty cells
+ *
+*/
+
+type RegionSpec = Array<string[]>
+
+class Row {
+  id: string;
+  constructor(id: string){
+    this.id = id.toString();
+  }
+}
+class Col {
+  id: string;
+  constructor(id: string){
+    this.id = id;
+  }
+}
+class Region {
+  id: string;
+
+  constructor(id: string){
+    this.id = id;
+  }
+}
+
+class Cell {
+  row: Row;
+  col: Col;
+  region: Region;
+  constructor(region: Region, row: Row, col: Col){
+    this.region = region;
+    this.row = row;
+    this.col = col;
+  }
+}
+
+export class Board {
+  size: number;
+  fillCount: number;
+  cells: Cell[];
+  rows: Row[];
+  cols: Col[];
+  regions: { [key: string]: Region };
+
+  constructor(regionSpec: RegionSpec, fillCount: number=1){
+    this.size = regionSpec.length;
+    this.fillCount = fillCount;
+    this.cells = [];
+    this.rows = [];
+    this.cols = [];
+    this.regions = {};
+    for(let i=0; i<this.size; i++){
+      this.rows.push(new Row(i.toString()));
+      this.cols.push(new Col(i.toString()));
+    }
+    for(let r=0; r<this.size; r++){
+      const row = this.rows[r];
+      const rowSpec = regionSpec[r];
+      if (rowSpec.length !== this.size){
+        throw new Error(`Invalid number of cells in row ${r}. There are ${this.size} rows, so expected ${this.size} cells in each row. Row has: ${rowSpec}`);
+      }
+      for(let c=0; c<this.size; c++){
+        const col = this.cols[c];
+        const regionId = rowSpec[c];
+        let region = this.regions[regionId];
+        if (!region){
+          region = new Region(regionId);
+          this.regions[regionId] = region;
+        }
+        this.cells.push(new Cell(region, row, col));
+      }
+    }
+    if (Object.keys(this.regions).length !== this.size){
+      throw new Error(`Invalid number of regions. Found ${Object.keys(this.regions)}, expected ${this.size}`);
+    }
+  }
 }
